68f14e2c7422d60c6917981f93462d6e12de0a74
ventamexy <venta.mexy@gmail.com>
2021-09-09 17:58:17 -0500

    fin de proyecto

---
+++ pages/itemData.js
@@ -0,0 +1,55 @@
+const tileData = [
+    {
+    img: 'https://inmobiliare.com/himalaya/wp-content/uploads/2019/08/trabajo-construccion.jpg',
+    title: 'Alyce Bass',
+    },
+    {
+    img: '',
+    title: 'Sabiha Plummer',
+    featured: true,
+    },
+    {
+      img: '',
+      title: 'Arthur Stewart',
+      featured: true,
+    },
+    {
+      img: '',
+      title: 'Hebe Harwood',
+    },
+    
+    {
+      img: '',
+      title: 'Keanu Hood',
+    },
+    {
+      img: '',
+      title: 'Leonardo Davila',
+    },
+    {
+      img: '',
+      title: 'Hayleigh Chapman',
+    },
+    {
+      img: '',
+      title: 'Reema English',
+    },
+    {
+      img: '',
+      title: 'Atticus Aldred',
+    },
+    {
+      img: '',
+      title: 'Cobie Morin',
+    },
+    {
+      img: '',
+      title: 'Corinne Mcghee',
+    },
+    {
+      img: '',
+      title: 'Ali Hendricks',
+    },
+  ];
+  
+  export default tileData;
